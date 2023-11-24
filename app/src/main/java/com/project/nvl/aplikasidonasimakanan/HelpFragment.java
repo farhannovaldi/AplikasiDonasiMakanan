460675326d426be6cdae20f13edc3c3221603cde
Farhan Novaldi <farhannovaldi002@gmail.com>
2023-11-24 19:03:52 +0700

    Penambahan menu laporan masalah

--- app/src/main/java/com/project/nvl/aplikasidonasimakanan/HelpFragment.java
+++ app/src/main/java/com/project/nvl/aplikasidonasimakanan/HelpFragment.java
@@ -1,64 +1,83 @@
 package com.project.nvl.aplikasidonasimakanan;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HelpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HelpFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText laporanMasalahInput;
+    private Button kirimLaporanButton;
+    private ProgressDialog progressBar;
+    private FirebaseAuth mAuth;
 
     public HelpFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HelpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HelpFragment newInstance(String param1, String param2) {
-        HelpFragment fragment = new HelpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        // Inisialisasi view
+        laporanMasalahInput = view.findViewById(R.id.laporanMasalahInput);
+        kirimLaporanButton = view.findViewById(R.id.kirimLaporanButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressBar = new ProgressDialog(getActivity());
+        progressBar.setTitle("Loading");
+        progressBar.setCancelable(false);
+
+        kirimLaporanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String laporanMasalah = laporanMasalahInput.getText().toString();
+
+                sendReportToDatabase(laporanMasalah);
+            }
+        });
+
+        return view;
+    }
+
+    private void sendReportToDatabase(String report) {
+
+        // Check id uid
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+
+            String userId = currentUser.getUid();
+
+            DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+            DatabaseReference laporanRef = database.child("laporan_masalah").child(userId).push();
+            laporanRef.setValue(report);
+
+            showToast("Laporan berhasil dikirim");
+        } else {
+            showToast("Silakan login terlebih dahulu");
+        }
+    }
+
+
+    private void showToast(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
     }
 }
